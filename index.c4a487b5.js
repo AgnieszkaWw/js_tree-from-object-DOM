bacecf20639f04a86218e97be1dd30158a787b0e
AgnieszkaWw <agnieszkawie82@onet.pl>
2024-01-23 16:38:05 +0100

    make build

---
+++ index.c4a487b5.js
@@ -0,0 +1,2 @@
+!function e(t,n){let r=document.createElement("ul");for(let l in t.appendChild(r),n)if(n.hasOwnProperty(l)){let t=document.createElement("li");t.textContent=l,r.appendChild(t),Object.keys(n[l]).length>0&&e(t,n[l])}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.c4a487b5.js.map
